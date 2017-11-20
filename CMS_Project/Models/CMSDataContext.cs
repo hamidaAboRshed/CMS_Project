609cb99a0030507db90b62ddecb75f890fa39236
RoulaArab <roula.arab95@gmail.com>
2017-11-20 20:31:54 +0200

    New_Database

--- CMS_Project/Models/CMSDataContext.cs
+++ CMS_Project/Models/CMSDataContext.cs
@@ -7,9 +7,9 @@ using System.Web;
 namespace CMS_Project.Models
 {
     public class CMSDataContext : DbContext
-    {
+    { 
         public CMSDataContext() : base("CMSConnection") {
-        }
+        } 
 
         public DbSet<MenuItem> MenuItems { get; set; }
 
---
+++ CMS_Project/Models/MenuItem_lang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS_Project.Models
+{
+    public class MenuItem_lang
+    {
+        public string Name { set; get; }
+        public MenuItem Menuitem { set; get; } 
+        public int? Menuitem_ID { set; get; }
+        public Language Lang {set; get;}
+        public int? Lang_ID { set; get; }
+       
+    } 
+}
