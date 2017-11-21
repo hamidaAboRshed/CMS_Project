bbb849cad6580132f71ef7514a7a4481cd4cef14
Walaa1994 <walaa.albaghdadi@hotmail.com>
2017-11-21 13:34:22 +0200

    itemLanguage

--- CMS_Project/Models/CMSDataContext.cs
+++ CMS_Project/Models/CMSDataContext.cs
@@ -16,6 +16,7 @@ namespace CMS_Project.Models
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<ITEM> ITEMs { get; set; }
+        public DbSet<item_lang> item_langs { get; set; }
 
         public DbSet<User> Users { get; set; }
 
@@ -31,6 +32,50 @@ namespace CMS_Project.Models
             HasOptional(e => e.Parent).
             WithMany().
             HasForeignKey(m => m.Parent_Id);
+
+            modelBuilder.Entity<Category_lang>()
+        .HasKey(bc => new { bc.category_ID, bc.Lang_ID });
+
+            modelBuilder.Entity<Category_lang>()
+                .HasRequired<Category>(bc => bc.category)
+                .WithMany()
+                .HasForeignKey(bc => bc.category_ID);
+
+            modelBuilder.Entity<Category_lang>()
+                .HasRequired<Language>(bc => bc.Lang)
+                .WithMany()
+                .HasForeignKey(bc => bc.Lang_ID);
+
+            //////////////////////
+
+            modelBuilder.Entity<item_lang>()
+        .HasKey(bc => new { bc.item_ID, bc.Lang_ID });
+
+            modelBuilder.Entity<item_lang>()
+                .HasRequired<ITEM>(bc => bc.item)
+                .WithMany()
+                .HasForeignKey(bc => bc.item_ID);
+
+            modelBuilder.Entity<item_lang>()
+                .HasRequired<Language>(bc => bc.Lang)
+                .WithMany()
+                .HasForeignKey(bc => bc.Lang_ID);
+
+            //////////////////////
+
+            modelBuilder.Entity<MenuItem_lang>()
+        .HasKey(bc => new { bc.Menuitem_ID, bc.Lang_ID });
+
+            modelBuilder.Entity<MenuItem_lang>()
+                .HasRequired<MenuItem>(bc => bc.Menuitem)
+                .WithMany()
+                .HasForeignKey(bc => bc.Menuitem_ID);
+
+            modelBuilder.Entity<MenuItem_lang>()
+                .HasRequired<Language>(bc => bc.Lang)
+                .WithMany()
+                .HasForeignKey(bc => bc.Lang_ID);
         }
+
     }
 }
